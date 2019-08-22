8b3690cec221647f3f8b370110fba3cfb1348be6
Julian <artjulius@hotmail.com>
2019-08-22 14:10:22 -0500

    cleaning code

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,16 +1,16 @@
 def bubble_sort_by array
-    n=array.length
+    n = array.length
     
     loop do
         move = false
-        aux= 0
+        aux = 0
         (n-1).times do |i|
-            test=yield(array[i],array[i+1])
-            if test>0
-                aux=array[i+1]
-                array[i+1]=array[i]
-                array[i]=aux
-                move=true
+            test = yield(array[i],array[i+1])
+            if test > 0
+                aux = array[i+1]
+                array[i+1] = array[i]
+                array[i] = aux
+                move = true
             end
         end
         
@@ -22,7 +22,7 @@ def bubble_sort_by array
     array
 end
 
-sorted=bubble_sort_by(["hi","hello","hey"]) do |left,right|
+sorted=bubble_sort_by(["hi","hello","hey","microverse","belmonte"]) do |left,right|
   left.length - right.length
 end
 p sorted
